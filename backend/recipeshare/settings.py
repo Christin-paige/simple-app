23490f5d88edadd60cb232ba4ded9a58d93bec68
Christin-paige <christinpmartin@gmail.com>
2024-08-08 14:20:54 -0400

    added cloudfront to static and media

--- backend/recipeshare/settings.py
+++ backend/recipeshare/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://d3kze3wmitbeb7.cloudfront.net/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_DIRS = [
  #   os.path.join(BASE_DIR, 'recipe'),  # Make sure this points to the correct folder
@@ -128,7 +128,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://d3kze3wmitbeb7.cloudfront.net/media/'
+
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
